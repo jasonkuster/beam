6731aeaa24fc61a7e667a3ad854d2e48f97cec10
Jason Kuster <jasonkuster@google.com>
2017-06-27 17:50:38 -0700

    no step
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .test-infra/jenkins/precommit_pipeline.groovy
+++ .test-infra/jenkins/precommit_pipeline.groovy
@@ -1,10 +1,8 @@
 #!groovy
 
 stage('Build') {
-    step {
-        node {
-            sh(script: "echo ${ghprbPullID}")
-        }
+    node {
+        sh(script: "echo ${ghprbPullID}")
     }
     // build job: 'beam_PreCommit_Build', parameters: [string(name: 'sha1', value: )]
 }
